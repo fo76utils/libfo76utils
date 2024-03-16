d5bfd01c0f70d1d25f7e2fa484d0ca569c6002b5
fo76utils <87907510+fo76utils@users.noreply.github.com>
2024-03-16 17:38:58 +0100

    SF_PBR_Tables: use R16G16B16A16_FLOAT output format, optimizations

--- src/pbr_lut.cpp
+++ src/pbr_lut.cpp
@@ -42,15 +42,16 @@ FloatVector4 SF_PBR_Tables::importanceSampleGGX(
   return h;
 }
 
-inline float SF_PBR_Tables::fresnel_n(float x)
+inline FloatVector8 SF_PBR_Tables::fresnel_n(FloatVector8 x)
 {
-  float   y = ((((-1.03202882f * x + 3.64690610f) * x - 5.46708684f) * x
-                + 4.84513712f) * x - 2.99292756f) * x + 1.0f;
+  FloatVector8  y(((((x * -1.03202882f + 3.64690610f) * x - 5.46708684f) * x
+                  + 4.84513712f) * x - 2.99292756f) * x + 1.0f);
   return y * y;
 }
 
 // ported from NifSkope fragment shader code, with modifications
 
+#if 0
 float SF_PBR_Tables::OrenNayarFull(
     float nDotL, float angleLN, float angleVN, float gamma, float roughness)
 {
@@ -95,24 +96,71 @@ float SF_PBR_Tables::OrenNayarFull(
 
   return l1 + l2;
 }
+#endif
+
+FloatVector4 SF_PBR_Tables::OrenNayar_1(
+    float nDotL, float angleLN, float angleVN, float gamma)
+{
+  float   alpha = std::max(angleVN, angleLN);
+  float   beta = std::min(angleVN, angleLN);
+
+  FloatVector4  g(nDotL);
+  if (gamma >= 0.0f)
+    g[1] = float(std::sin(alpha));
+  else
+    g[1] = float(std::sin(alpha)) - (beta * beta * beta * 0.258012275f);
+
+  float   powValue = alpha * beta * 0.405284735f;
+  g[2] = powValue * powValue;
+
+  // Avoid asymptote at pi/2
+  float   asym = 1.57079633f;
+  float   lim1 = asym + 0.005f;
+  float   lim2 = asym - 0.005f;
+
+  float   ab2 = (alpha + beta) * 0.5f;
+
+  beta = (beta < asym ? std::min(beta, lim2) : std::max(beta, lim1));
+  ab2 = (ab2 < asym ? std::min(ab2, lim2) : std::max(ab2, lim1));
+
+  // Reflection
+  g[1] = g[1] * nDotL * gamma * float(std::tan(beta));
+  g[2] = g[2] * nDotL * (1.0f - float(std::fabs(gamma))) * float(std::tan(ab2));
+
+  // Interreflection
+  float   twoBetaPi = beta * 0.636619772f;
+  g[3] = nDotL * (1.0f - (gamma * twoBetaPi * twoBetaPi));
+
+  return g;
+}
+
+float SF_PBR_Tables::OrenNayar_2(FloatVector4 g, float roughness)
+{
+  float   roughnessSquared = roughness * roughness;
+  float   roughnessSquared9 = (roughnessSquared / (roughnessSquared + 0.09f));
+
+  // C1, C2, and C3
+  float   c1 = 1.0f - 0.5f * (roughnessSquared / (roughnessSquared + 0.33f));
+  float   c2 = 0.45f * roughnessSquared9;
+  float   c3 = 0.125f * roughnessSquared9;
+
+  // Reflection
+  float   l1 = (g[0] * c1) + (g[1] * c2) + (g[2] * c3);
+
+  // Interreflection
+  float   l2 = g[3] * 0.17f * (roughnessSquared / (roughnessSquared + 0.13f));
+
+  return l1 + l2;
+}
 
 void SF_PBR_Tables::threadFunction(
-    unsigned char *outBuf, int width, int y0, int y1)
+    unsigned char *outBuf, int width, int nSpec, int y0, int y1,
+    const FloatVector4 *coordTable_D)
 {
-  std::vector< FloatVector4 > coordTable_D(256, FloatVector4(0.0f));
-  std::vector< FloatVector4 > coordTable_S(4096, FloatVector4(0.0f));
-  for (int i = 0; i < int(coordTable_D.size()); i++)
-  {
-    FloatVector4  xi(SF_PBR_Tables::Hammersley(i, int(coordTable_D.size())));
-    double  phi = double(xi[0]) * 6.28318530717959;
-    double  cosTheta = xi[1];
-    double  sinTheta = std::sqrt(1.0 - (cosTheta * cosTheta));
-    coordTable_D[i][0] = float(std::cos(phi) * sinTheta);
-    coordTable_D[i][1] = float(std::sin(phi) * sinTheta);
-    coordTable_D[i][2] = xi[1];
-    coordTable_D[i][3] = float(std::acos(xi[1]));
-  }
-  outBuf = outBuf + (size_t(y0) * size_t(width) * sizeof(std::uint32_t));
+  nSpec = (nSpec + 7) & ~7;
+  std::vector< FloatVector8 > coordTable_S(size_t(nSpec) >> 2,
+                                           FloatVector8(0.0f));
+  outBuf = outBuf + (size_t(y0) * size_t(width) * sizeof(std::uint64_t));
   for (int y = y0; y < y1; y++)
   {
     FloatVector4  normal(0.0f, 0.0f, 1.0f, 0.0f);
@@ -120,75 +168,82 @@ void SF_PBR_Tables::threadFunction(
     float   a = roughness * roughness;
     float   a2 = a * a;
     float   k = a * 0.5f;
-    int     n = int(coordTable_S.size());
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < nSpec; i++)
     {
-      FloatVector4  xi(SF_PBR_Tables::Hammersley(i, n));
-      coordTable_S[i] = SF_PBR_Tables::importanceSampleGGX(xi, normal, a2);
-      coordTable_S[i][3] = std::min(std::max(coordTable_S[i][2], 0.0f), 1.0f);
+      FloatVector4  xi(Hammersley(i, nSpec));
+      FloatVector4  tmp(importanceSampleGGX(xi, normal, a2));
+      coordTable_S[(i & ~7) >> 2][i & 7] = tmp[0];
+      coordTable_S[((i & ~7) >> 2) + 1][i & 7] = tmp[2];
     }
-    for (int x = 0; x < width; x++, outBuf = outBuf + sizeof(std::uint32_t))
+    for (int x = 0; x < width; x++, outBuf = outBuf + sizeof(std::uint64_t))
     {
       float   nDotV = (float(x) + 0.5f) / float(width);
-      float   g_v = 1.0f / (nDotV * (1.0f - k) + k);
       FloatVector4  v(float(std::sqrt(1.0f - (nDotV * nDotV))), 0.0f, nDotV,
                       float(std::acos(nDotV)));
-      FloatVector4  s(0.0f);
-      for (int i = 0; i < n; i++)
+      FloatVector8  s1(0.0f);
+      FloatVector8  s2(0.0f);
+      FloatVector8  v_x(v[0]);
+      FloatVector8  v_z(v[2]);
+      for (int i = 0; (i + 8) <= nSpec; i = i + 8)
       {
-        FloatVector4  h(coordTable_S[i]);
-        FloatVector4  tmp(h * v);
-        float   vDotH = tmp[0] + tmp[2];
-        float   nDotL = h[2] * (2.0f * vDotH) - v[2];
-        if (!(nDotL > 0.0f))
-          continue;
-        vDotH = std::min(std::max(vDotH, 0.0f), 1.0f);
-        float   nDotH = h[3];
-        float   g = g_v * nDotL * vDotH / ((nDotL * (1.0f - k) + k) * nDotH);
-        float   f = fresnel_n(vDotH);
-        s += (FloatVector4(0.0f, 1.0f - f, f, 0.0f) * g);
+        FloatVector8  h_x(coordTable_S[i >> 2]);
+        FloatVector8  nDotH(coordTable_S[(i >> 2) + 1]);        // h_z
+        FloatVector8  vDotH((h_x * v_x) + (nDotH * v_z));
+        FloatVector8  nDotL(nDotH * (vDotH + vDotH) - v_z);
+        nDotH.maxValues(FloatVector8(0.0f));
+        vDotH.maxValues(FloatVector8(0.0f));
+        nDotL.maxValues(FloatVector8(0.0f));
+        FloatVector8  g(nDotL * vDotH
+                        / ((nDotL * (FloatVector8(1.0f) - k) + k) * nDotH));
+        FloatVector8  f(fresnel_n(vDotH));
+        s1 += (f * g);
+        s2 += g;
       }
-      s /= float(n);
-      s[1] = s[1] + s[2];
-      s[2] = s[2] / s[1];
-      for (int i = 0; i < int(coordTable_D.size()); i++)
-      {
-        FloatVector4  l(coordTable_D[i]);
-        float   gamma = l.dotProduct2(l);
-        if (gamma > 0.000001f)          // gamma = cos(phi_L - phi_V), phi_V = 0
-          gamma = l[0] / float(std::sqrt(gamma));
-        s[0] += SF_PBR_Tables::OrenNayarFull(l[2], l[3], v[3], gamma,
-                                             roughness);
-        s[3] += l[2];
-      }
-      s[0] = (s[0] / s[3]) * 2.0f - 1.25f;
-      s[3] = 1.0f;
-      s *= 255.0f;
-      FileBuffer::writeUInt32Fast(outBuf, s.convertToA2R10G10B10());
+      FloatVector4  s(s1.dotProduct(FloatVector8(1.0f)),
+                      s2.dotProduct(FloatVector8(1.0f)), 0.0f, 0.0f);
+      s /= (float(nSpec) * (nDotV * (1.0f - k) + k));
+      s[0] = s[0] / s[1];
+      s[2] = OrenNayar_2(coordTable_D[x], roughness);
+      s[3] = fresnel_n(FloatVector8(nDotV))[0];
+      FileBuffer::writeUInt64Fast(outBuf, s.convertToFloat16());
     }
   }
 }
 
-SF_PBR_Tables::SF_PBR_Tables(int width)
-  : imageData(size_t(width) * size_t(width) * sizeof(std::uint32_t) + 148, 0)
+SF_PBR_Tables::SF_PBR_Tables(int width, int nSpec, int nDiff)
+  : imageData(size_t(width) * size_t(width) * sizeof(std::uint64_t) + 148, 0)
 {
   unsigned char *p = imageData.data();
-  FileBuffer::writeUInt32Fast(p, 0x20534444U);          // "DDS "
-  p[4] = 124;                           // size of header
-  FileBuffer::writeUInt32Fast(p + 8, 0x0002100FU);      // flags
-  // height, width, pitch
-  FileBuffer::writeUInt32Fast(p + 12, std::uint32_t(width));
-  FileBuffer::writeUInt32Fast(p + 16, std::uint32_t(width));
-  FileBuffer::writeUInt32Fast(p + 20,
-                              std::uint32_t(width * sizeof(std::uint32_t)));
-  p[28] = 1;                            // number of mipmaps
-  p[76] = 32;                           // size of pixel format
-  p[80] = 0x04;                         // DDPF_FOURCC
-  FileBuffer::writeUInt32Fast(p + 84, 0x30315844U);     // "DX10"
-  FileBuffer::writeUInt32Fast(p + 108, 0x00401008U);    // dwCaps
-  p[128] = 0x18;                        // DXGI_FORMAT_R10G10B10A2_UNORM
-  p[132] = 3;                           // DDS_DIMENSION_TEXTURE2D
+  // DXGI_FORMAT_R16G16B16A16_FLOAT
+  FileBuffer::writeDDSHeader(p, 0x0A, width, width);
   p = p + 148;
+
+  std::vector< FloatVector4 > coordTable_D(size_t(width), FloatVector4(0.0f));
+  for (int x = 0; x < width; x++)
+  {
+    float   nDotV = (float(x) + 0.5f) / float(width);
+    FloatVector4  v(float(std::sqrt(1.0f - (nDotV * nDotV))), 0.0f, nDotV,
+                    float(std::acos(nDotV)));
+    float   weight = 0.0f;
+    for (int i = 0; i < nDiff; i++)
+    {
+      FloatVector4  xi(Hammersley(i, nDiff));
+      double  phi = double(xi[0]) * 6.28318530717959;
+      double  cosTheta = xi[1];
+      double  sinTheta = std::sqrt(1.0 - (cosTheta * cosTheta));
+      FloatVector4  l;
+      l[0] = float(std::cos(phi) * sinTheta);
+      l[1] = float(std::sin(phi) * sinTheta);
+      l[2] = xi[1];
+      l[3] = float(std::acos(xi[1]));
+      float   gamma = l.dotProduct2(l);
+      if (gamma > 0.000001f)            // gamma = cos(phi_L - phi_V), phi_V = 0
+        gamma = l[0] / float(std::sqrt(gamma));
+      coordTable_D[x] += OrenNayar_1(l[2], l[3], v[3], gamma);
+      weight += l[2];
+    }
+    coordTable_D[x] /= weight;
+  }
 
   int     threadCnt = int(std::thread::hardware_concurrency());
   threadCnt = std::min< int >(std::max< int >(threadCnt, 1), 16);
@@ -200,9 +255,10 @@ SF_PBR_Tables::SF_PBR_Tables(int width)
   {
     for (int i = 0; i < threadCnt; i++)
     {
-      threads[i] = new std::thread(threadFunction, p, width,
+      threads[i] = new std::thread(threadFunction, p, width, nSpec,
                                    i * width / threadCnt,
-                                   (i + 1) * width / threadCnt);
+                                   (i + 1) * width / threadCnt,
+                                   coordTable_D.data());
     }
     for (int i = 0; i < threadCnt; i++)
     {
--- src/pbr_lut.hpp
+++ src/pbr_lut.hpp
@@ -4,6 +4,7 @@
 
 #include "common.hpp"
 #include "fp32vec4.hpp"
+#include "fp32vec8.hpp"
 
 class SF_PBR_Tables
 {
@@ -13,18 +14,22 @@ class SF_PBR_Tables
   static FloatVector4 importanceSampleGGX(FloatVector4 xi, FloatVector4 normal,
                                           float a2);    // pow(roughness, 4)
   // approximates Fresnel function for n = 1.5, normalized to 0.0 to 1.0
-  static inline float fresnel_n(float x);
+  static inline FloatVector8 fresnel_n(FloatVector8 x);
   // gamma = cos(phi_L - phi_V)
-  static float OrenNayarFull(float nDotL, float angleLN, float angleVN,
-                             float gamma, float roughness);
-  static void threadFunction(unsigned char *outBuf, int width, int y0, int y1);
+  static FloatVector4 OrenNayar_1(float nDotL, float angleLN, float angleVN,
+                                  float gamma);
+  // g = return value of OrenNayar_1()
+  static float OrenNayar_2(FloatVector4 g, float roughness);
+  static void threadFunction(unsigned char *outBuf, int width, int nSpec,
+                             int y0, int y1, const FloatVector4 *coordTable_D);
  public:
-  SF_PBR_Tables(int width = 512);
+  SF_PBR_Tables(int width = 512, int nSpec = 4096, int nDiff = 1024);
   // texture U = N·V
   // texture V = roughness
   // R = indirect specular F
   // G = indirect specular G
-  // B = indirect diffuse scale * 2.0 - 1.25
+  // B = indirect diffuse scale
+  // A = normalized Fresnel function without roughness
   inline const std::vector< unsigned char >& getImageData() const
   {
     return imageData;
