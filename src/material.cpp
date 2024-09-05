e25fb20cfc0d24bfa20855c85d8ea83e878c55e7
fo76utils <87907510+fo76utils@users.noreply.github.com>
2024-09-06 00:58:50 +0200

    Revert "CE2MaterialDB: changed default material color override mode to Lerp"
    This reverts commit 713bba5790a953c68410f8c6204e6197d746ff85.

--- src/material.cpp
+++ src/material.cpp
@@ -293,7 +293,6 @@ CE2MaterialObject * CE2MaterialDB::findMaterialObject(
       {
         CE2Material::Material *q = static_cast< CE2Material::Material * >(o);
         q->color = FloatVector4(1.0f);
-        q->colorModeFlags = 1;          // "Lerp"
         q->flipbookColumns = 1;
         q->flipbookRows = 1;
         q->flipbookFPS = 30.0f;
